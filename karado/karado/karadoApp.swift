b696a2c8dab371bf87f530a29afb9a577df4eff9
karado-gif <redytogomyg@proton.me>
2025-06-19 17:41:30 +0700

    Initial commit

---
+++ karado/karado/karadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  karadoApp.swift
+//  karado
+//
+//  Created by Anatolii Anatolii on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct karadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ karado/karadoTests/karadoTests.swift
@@ -0,0 +1,16 @@
+//
+//  karadoTests.swift
+//  karadoTests
+//
+//  Created by Anatolii Anatolii on 19.06.2025.
+//
+
+import Testing
+
+struct karadoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
